9ba2121565c5a2cfb1a507c9b6729ac1134fa60e
slhuang520 <349733050@qq.com>
2019-06-07 18:50:49 +0800

    add the VUE CLI with jquery and bootstrap

---
+++ cli/my-vue-cli/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <app-header></app-header>
+    <hr>
+    <div class="row">
+      <app-server></app-server>
+      <app-server-status></app-server-status>
+    </div>
+    <hr>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+ // import $ from 'jquery'; // 这里可以不用再引入了
+  import serverStatus from './components/server/serverStatus.vue';
+  import server from './components/server/server.vue';
+  export default {
+    components: { // 定义局部组件
+      'app-server': server,
+      'app-server-status': serverStatus
+    }
+  };
+</script>
---
+++ cli/my-vue-cli/src/components/common/header.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="row">
+    <div class="col-xs-12">
+      <header>
+        <h1>服务器状态</h1>
+      </header>
+    </div>
+  </div>
+</template>
