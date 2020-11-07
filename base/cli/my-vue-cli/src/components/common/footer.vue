51d19ccffd0b59b6a2c0fe70d631f72abd16527c
slhuang520 <349733050@qq.com>
2020-11-07 13:55:08 +0800

    del cli under base dirctory for duplicate

--- base/cli/my-vue-cli/src/components/common/footer.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="row">
-    <div class="col-xs-12">
-      <span>xxxx有限责任公司</span>
-    </div>
-  </div>
-</template>
--- base/cli/my-vue-cli/src/components/common/header.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="row">
-    <div class="col-xs-12">
-      <header>
-        <h1>服务器状态</h1>
-      </header>
-    </div>
-  </div>
-</template>
